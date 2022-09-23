d4ec296dd00be82eefdfc067fb797662aa46c3d9
Brian Gee <briangeertsma@gmail.com>
2022-09-22 22:44:44 -0400

    initial working commit

---
+++ iphoneModels.js
@@ -0,0 +1,37 @@
+// iPhone 14 Pro
+
+// 128GB Deep Purple MQ0E3LL/A
+// 256GB Deep Purple MQ1D3LL/A
+// 512GB Deep Purple MQ273LL/A
+// 1TB Deep Purple MQ303LL/A
+// 128GB Gold MQ063LL/A
+// 256GB Gold MQ163LL/A
+// 512GB Gold MQ213LL/A
+// 1TB Gold MQ2T3LL/A
+// 128GB Silver MQ003LL/A
+// 256GB Silver MQ0X3LL/A
+// 512GB Silver MQ1U3LL/A
+// 1TB Silver MQ2L3LL/A
+// 128GB Space Black MPXT3LL/A
+// 256GB Space Black MQ0N3LL/A
+// 512GB Space Black MQ1K3LL/A
+// 1TB Space Black MQ2E3LL/A
+
+// iPhone 14 Pro Max
+
+// 128GB Deep Purple MQ8R3LL/A
+// 256GB Deep Purple MQ8W3LL/A
+// 512GB Deep Purple MQ913LL/A
+// 1TB Deep Purple MQ953LL/A
+// 128GB Gold MQ8Q3LL/A
+// 256GB Gold MQ8V3LL/A
+// 512GB Gold MQ903LL/A
+// 1TB Gold MQ943LL/A
+// 128GB Silver MQ8P3LL/A
+// 256GB Silver MQ8U3LL/A
+// 512GB Silver MQ8Y3LL/A
+// 1TB Silver MQ933LL/A
+// 128GB Space Black MQ8N3LL/A
+// 256GB Space Black MQ8T3LL/A
+// 512GB Space Black MQ8X3LL/A
+// 1TB Space Black MQ923LL/A
---
+++ stockCheck.js
@@ -0,0 +1,58 @@
+let nodemailer = require('nodemailer');
+let dotenv = require("dotenv");
+const phones = ['MPXT3LL/A', 'MQ0N3LL/A']
+dotenv.config();
+const MAIL_USER = process.env.MAIL_USER;
+const MAIL_PASS = process.env.MAIL_PASS;
+
+sendEmail = (product) => {
+    let emailList = ['brian86753099@gmail.com', 'nat_rez@icloud.com']
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: MAIL_USER,
+          pass: MAIL_PASS
+        }
+      });
+
+      let mailOptions = {
+        from: 'theonebot77@gmail.com',
+        to: emailList,
+        subject: 'iPhone Availability',
+        text: ( product + 'is available!')
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+};
+
+checkPhone = (part) => {
+    const url = 'https://www.apple.com/shop/fulfillment-messages?parts.0=' + part + '&store=R087'
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        const productName = data.body.content.pickupMessage.stores[0].partsAvailability[part].messageTypes.regular.storePickupProductTitle;
+        const productAvailable = data.body.content.pickupMessage.stores[0].partsAvailability[part].pickupDisplay;
+
+        let status;
+        if (productAvailable === "available")  {
+            status = '✔️';
+          sendEmail(productName)
+        };
+        if (productAvailable === "unavailable") {
+            status = '✖️';
+        };
+
+        console.log(productName+ " - " + status);
+    });
+};
+
+
+phones.forEach(checkPhone);
